95ec994238c781e8911bf39dc48f408b749027aa
kevin_garay <kgaray@applaudostudios.dev>
2022-10-16 13:47:06 -0500

    user signup page

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+  # users log in 
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
 end
