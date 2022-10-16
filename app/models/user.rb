dae9e0400cc629654639b2bfece670278c1b3c2f
kevin_garay <kgaray@applaudostudios.dev>
2022-10-16 13:04:13 -0500

    add one to many association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 
 # User ckass
 class User < ApplicationRecord
+  has_many :articles
+  
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
                        length: { minimum: 3, maximum: 25 }
