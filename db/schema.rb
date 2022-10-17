49a10ba6af9486adf05b549a983b20da0c7bc6ad
kevin_garay <kgaray@applaudostudios.dev>
2022-10-17 13:10:55 -0500

    added admin column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_16_182321) do
+ActiveRecord::Schema.define(version: 2022_10_17_180707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_10_16_182321) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
