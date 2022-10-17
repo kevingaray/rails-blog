0c0ad2392b9f8ca48d4f2db79af166d38fa2360f
kevin_garay <kgaray@applaudostudios.dev>
2022-10-17 12:44:20 -0500

    add restrictions to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,8 @@
 # articles controller
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show; end
 
@@ -50,4 +52,13 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :description)
   end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
+    end
+  end
+
+  
 end
