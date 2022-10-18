20055cb3c76ed12cecc6abdc3e4976209ffe5213
kevin_garay <kgaray@applaudostudios.dev>
2022-10-18 08:20:10 -0500

    admin create category test

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -3,6 +3,12 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin = User.create(username: 'johndoe', email: 'johndoe@example.com',
+                         password: 'password', admin: true)
+    sign_in_as(@admin)
+  end
+
   test 'get new category form and create category' do
     get '/categories/new'
     assert_response :success
@@ -15,15 +21,14 @@ class CreateCategoryTest < ActionDispatch::IntegrationTest
     assert_match 'Sports', response.body
   end
 
-  test "get new category form and reject invalid category submission" do
-    get "/categories/new"
+  test 'get new category form and reject invalid category submission' do
+    get '/categories/new'
     assert_response :success
     assert_no_difference 'Category.count' do
-      post categories_path, params: { category: { name: " "} }
+      post categories_path, params: { category: { name: ' ' } }
     end
-    assert_match "errors", response.body
+    assert_match 'errors', response.body
     assert_select 'div.alert'
     assert_select 'h4.alert-heading'
   end
-
 end
