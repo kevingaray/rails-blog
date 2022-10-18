e1ba91bdb0c5f11008a28dd41176bbf712e65fd6
kevin_garay <kgaray@applaudostudios.dev>
2022-10-18 07:27:04 -0500

    create category and test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,9 +2,28 @@
 
 # caterogires controller class
 class CategoriesController < ApplicationController
-  def new; end
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
 
   def index; end
 
   def show; end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+  
 end
